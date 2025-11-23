0f804ab335aa9c910837dde6c8f3f194374cf57c
Stas <staspert3@gmail.com>
2025-11-23 13:13:22 +0300

    sdsdsdsd

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({ static: null }); // ОТКЛЮЧАЕТ public/
+const middlewares = jsonServer.defaults(); // ВАЖНО: без параметров
 
 server.use(middlewares);
 server.use(router);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log("JSON Server is running on port", PORT);
+  console.log('JSON Server is running on port', PORT);
 });
